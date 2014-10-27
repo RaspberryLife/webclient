c131335d625f6a66ba05bd1dcd856e68e97bdd26
Hadi Skeini <hadi.skeini@gmail.com>
2014-10-27 02:44:59 +0100

    html declaration, indentation

--- js/dynamiccontent.js
+++ js/dynamiccontent.js
@@ -1,39 +1,39 @@
 $(function() {
 
-    if(Modernizr.history){
-        var newHash      = "",
-        $mainContent = $("#main-content"),
-        $el;
+  if(Modernizr.history){
+    var newHash      = "",
+    $mainContent = $("#main-content"),
+    $el;
 
     $("nav").delegate("a", "click", function() {
-        _link = $(this).attr("href");
-        history.pushState(null, null, _link);
-        loadContent(_link);
-        return false;
+      _link = $(this).attr("href");
+      history.pushState(null, null, _link);
+      loadContent(_link);
+      return false;
     });
 
     function loadContent(href){
-        $mainContent
-                .find("#guts")
-                .fadeOut(200, function() {
-                    $mainContent.hide().load(href + " #guts", function() {
-                        $mainContent.fadeIn(200, function() {
-                        });
-                        $("nav li").removeClass("active");
-                        console.log(href+" loaded");
-                        $("nav a[href$='"+href+"']").parent().addClass("active");
-                    });
-                });
-
-                if(href=="debug.html"){
-                  console.log("yeah!");
-                  getScript("js/debug.js");
-                }
+      $mainContent
+      .find("#guts")
+      .fadeOut(200, function() {
+        $mainContent.hide().load(href + " #guts", function() {
+          $mainContent.fadeIn(200, function() {
+          });
+          $("nav li").removeClass("active");
+          console.log(href+" loaded");
+          $("nav a[href$='"+href+"']").parent().addClass("active");
+        });
+      });
+      
+      if(href=="debug.html"){
+        console.log("yeah!");
+        getScript("js/debug.js");
+      }
     }
 
     $(window).bind('popstate', function(){
-       _link = location.pathname.replace(/^.*[\\\/]/, '');
-       loadContent(_link);
+      _link = location.pathname.replace(/^.*[\\\/]/, '');
+      loadContent(_link);
     });
-}
+  }
 });
