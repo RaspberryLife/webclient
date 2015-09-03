2581e6e1497ef96c4b0a0ac91e517720223111f9
Peter Mösenthin <peter.moesenthin@gmail.com>
2015-09-03 22:38:27 +0200

    remove unused protobuf code and shirnk webapp for fablab scenario

--- js/fablab.js
+++ js/fablab.js
@@ -2,35 +2,20 @@ var connection_status_text = $('#connection_status');
 
 var checkConnection = function checkConnection() {
 	console.log("Checking connection");
-	if (socket.readyState === WebSocket.OPEN) {
-		connection_status_text.html('OK');
-	} else {
-		connection_status_text.html('Not connected');
-	}
+	$.ajax({
+		url: "http://localhost:8080/rbl/extension/fablab/status",
+
+	}).success(function(response) {
+		//Logs Fablab module states
+		connection_status_text.html(response.status);
+	});
 };
-
-function switchOn() {
-
-	var clientID = "webclient v1.0";
-	var msgType = "RUN_INSTRUCTION";
-	var msgFlag = "REQUEST";
-	var msgNumber = "53";
-	var actType = "MODULE";
-	var actID = "1";
-	var insId = "1";
-	var paramsstr = "1,1,4";
-	var params = paramsstr.split(',');
-	var modType = "MODULE_OUTLET";
-	var modId = "1";
-
-	sendInstructionMessage(clientID, msgType, msgFlag, msgNumber, actType, actID, insId, params, modType, modId);
-}
 
 $(':input').change(function () {
 	console.log($(this).data('module-name') + " is " + ($(this).prop('checked') ? 'on' : 'off'));
 
 	if ($(this).prop('checked') == true) {
-		switchOn();
+		//switch light on
 	}
 
 });
@@ -39,9 +24,6 @@ setInterval(function () {
 		checkConnection();
 	}
 	, 3000);
-
-createWebSocket("localhost");
-sendAuthRequestMessage("abc12345");
 
 //getListOfModules();
 //load switches
